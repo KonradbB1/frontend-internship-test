af3f054258d3f1573fb561862f3af6ea0ed09ad9
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-06-10 18:16:50 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,31 @@
 /* Here goes your JS code */
+
+const popup = document.querySelector(".popup"),
+      openPopup = document.getElementById("show-popup-form"),
+      closePopup = document.querySelector(".close-popup"),
+      btnForm = document.querySelector(".btn-form");
+
+openPopup.onclick = () => {
+  popupBox();
+};
+
+closePopup.onclick = () => {
+  popupBox();
+};
+
+popup.onclick = (event) => {
+  if (event.target == popup) {
+    popupBox();
+  }
+};
+
+const popupBox = () => {
+  popup.classList.toggle("open");
+};
+
+btnForm.onclick = () => {
+    
+      openPopup.classList.add("hide");
+
+};
+
