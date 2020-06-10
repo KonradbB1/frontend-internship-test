5cf239848c2a9a43af5044ef75ce43d27340d8b9
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-06-10 18:29:12 +0200

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -24,8 +24,44 @@ const popupBox = () => {
 };
 
 btnForm.onclick = () => {
-    
+      if(validate() === false) {
+        validate();
+      }
+
       openPopup.classList.add("hide");
 
 };
 
+const validate = () => {
+  const passInput = document.querySelector('.password').value,
+      mailInput = document.querySelector('.email').value,
+      errorInput = document.querySelector('.error-message'),
+      checkBox = document.getElementById('checkbox-form');
+
+  let text,
+      regexp = new RegExp('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}', 'gi'),
+      passwordRegexp = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$','mgi');
+
+  if(mailInput.length < 8 || regexp.test(mailInput) === false) {
+      text= "Please enter valid e-mail";
+      errorInput.innerHTML = text;
+      return false;
+  }
+
+  if(passwordRegexp.test(passInput) === false) {
+      text= "Please enter valid password(Example:Tomek12@)";
+      errorInput.innerHTML = text;
+      return false;
+  }
+
+  if(checkBox.checked === false) {
+    text="Please accept our rules";
+    errorInput.innerHTML = text;
+    return false;
+  }
+  text = "Successfull, logging in";
+  errorInput.innerHTML = text;
+  errorInput.style.backgroundColor= "green";
+  errorInput.style.color= "white";
+  return true;
+}
